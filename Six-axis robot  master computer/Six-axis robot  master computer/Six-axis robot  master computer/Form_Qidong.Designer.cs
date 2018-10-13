c381930ea96faafa4772d3ca20f7a6c94b552769
Lanhonghao <Lanhonghao@126.com>
2018-10-13 18:53:44 +0800

    Add files via upload

---
+++ Six-axis robot  master computer/Six-axis robot  master computer/Six-axis robot  master computer/Form_Qidong.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace Six_axis_robot__master_computer
+{
+    partial class Form_Qidong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Qidong));
+            this.SuspendLayout();
+            // 
+            // Form_Qidong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Six_axis_robot__master_computer.Properties.Resources.实验室_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(256, 256);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form_Qidong";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MOLAB机械创新实验室";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form_Qidong_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
